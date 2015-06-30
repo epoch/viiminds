79998bb0ce5859435e51f8b9e1596e006f831448
Daniel Tsui <tsui.daniel@gmail.com>
2015-06-30 11:06:26 +1000

    the very helpful javascript

---
+++ 01-javascript/js_intro/js_intro.js
@@ -0,0 +1,41 @@
+console.log('its working');
+
+var myName = 'james';
+var myComputerName = 'xps';
+
+console.log(myName.toUpperCase());
+
+// var yourName = prompt('please enter your name');
+// var favouriteColour = prompt('please enter your favourite Colour');
+
+// console.log('hello ' + yourName + ', I like ' + favouriteColour + ' too');
+
+// this is a comment
+
+if (5 > 4) {
+  console.log('maths is so easy!');
+}
+
+var viiMindsRule = true;
+
+if (viiMindsRule) {
+  console.log('yay viiminds!');
+}
+
+// values that are false-y false, the empty string "", the number 0
+// undefined, null
+
+var name = '';
+if (name) {
+  console.log('hello ' + name);
+}
+
+var points = 0;
+if (points) {
+  console.log('you have ' + points);
+}
+
+var firstname;
+if (firstname) {
+  console.log('your firstname is ' + firstname);
+}
