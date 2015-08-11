1b44982792fafd07cbd33602e13d9dc820473bf8
Daniel Tsui <tsui.daniel@gmail.com>
2015-08-11 11:03:35 +1000

    for aidan

--- 07-testing/todor/main.rb
+++ 07-testing/todor/main.rb
@@ -1,5 +1,12 @@
-     
 require 'sinatra'
+require 'sinatra/reloader'
+
+require_relative 'database_config'
+require_relative 'task'
+
+after do
+  ActiveRecord::Base.connection.close
+end
 
 get '/' do
   @tasks = Task.all
