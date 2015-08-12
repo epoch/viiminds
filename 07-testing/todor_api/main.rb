99c8b99583f972e2b51cc7b12d37e5b41732c3f5
Daniel Tsui <tsui.daniel@gmail.com>
2015-08-12 17:00:36 +1000

    refactored

--- 07-testing/todor_api/main.rb
+++ 07-testing/todor_api/main.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pry'
 
 require_relative 'database_config'
 require_relative 'task'
@@ -8,11 +9,33 @@ after do
   ActiveRecord::Base.connection.close
 end
 
-get '/todos.json' do
+before do
   content_type :json
   response.headers['Access-Control-Allow-Origin'] = '*'
+  response.headers['Access-Control-Allow-Methods'] = 'PUT'
+  response.headers["Allow"] = "HEAD,GET,PUT,POST,DELETE,OPTIONS"
+  response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
+end
+
+get '/todos.json' do
   @tasks = Task.all
   @tasks.to_json
+end
+
+get '/random_task' do
+  @task = Task.all.sample
+  @task.to_json  
+end
+
+options "*" do
+  200
+end
+
+put '/todos/:id' do
+  @task = Task.find params[:id]
+  # @task.body = params[:body]
+  @task.completed = !@task.completed
+  @task.save
 end
 
 
